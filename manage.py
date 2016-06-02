46be1360ffa3306c59b55bb137e83129f30dedbc
Daniel R. Dugan <sound2525@gmail.com>
2016-06-02 18:32:09 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-    
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+heroku config:set DISABLE_COLLECTSTATIC=1
